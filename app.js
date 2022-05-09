fd13f2d186f5c4e09f22c0eff796a826716b299e
Ladan Nazari <ladannazary@gmail.com>
2022-05-10 00:03:55 +0430

    project skeleton

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
+// this isn't touched
 
 // view engine setup
 app.set('view engine', 'pug');
