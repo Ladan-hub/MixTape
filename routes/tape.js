7410ab1af3f008794b606ad7aaff737f29ea0fc3
Joyce <qyliu10@gmail.com>
2022-05-13 19:28:17 -0700

    update tape name solved

--- routes/tape.js
+++ routes/tape.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 const { requireAuth } = require('../auth');
 const { csrfProtection, asyncHandler } = require('./utils');
 const db = require('../db/models');
@@ -13,6 +14,11 @@ const taskNotFoundError = (id) => {
     return err;
 };
 
+// const inputValidators = [
+//     check('username')
+//         .exists({ checkFalsy: true })
+//         .withMessage('Please provide a valid value for Tape Name')
+// ];
 
 // read a list of tapes
 router.get('/users/:id/tapes', asyncHandler(async (req, res) => {
@@ -34,14 +40,17 @@ router.get('/users/:id/tapes', asyncHandler(async (req, res) => {
 }));
 
 // read all songs in a single tape
-router.get('/users/:userId/tapes/:tapeId', asyncHandler(async (req, res) => {
+router.get('/users/:userId/tapes/:tapeId', csrfProtection, asyncHandler(async (req, res) => {
 
     const userId = req.params.userId;
     const tapeId = req.params.tapeId;
 
-    const tape = await db.Tape.findOne({ where: {
-        id: tapeId
-    }})
+
+    const tape = await db.Tape.findOne({
+        where: {
+            id: tapeId
+        }
+    })
     const playlists = await db.Playlist.findAll({
 
         include: [{ model: db.Song },
@@ -53,9 +62,9 @@ router.get('/users/:userId/tapes/:tapeId', asyncHandler(async (req, res) => {
         }]
     });
 
-    if(playlists){
-        res.render('tapes-2', {playlists, tape})
-    }else{
+    if (playlists) {
+        res.render('tapes-2', { playlists, tape, userId, csrfToken: req.csrfToken()})
+    } else {
         next(taskNotFoundError(req.params.id));
     }
 
@@ -63,7 +72,7 @@ router.get('/users/:userId/tapes/:tapeId', asyncHandler(async (req, res) => {
 }))
 
 // create a new tape, add a new tape button
-router.post('/users/:id/tapes', asyncHandler(async (req, res) => {
+router.post('/users/:id/tapes', csrfProtection, asyncHandler(async (req, res) => {
 
     const userId = req.params.id;
 
@@ -73,5 +82,33 @@ router.post('/users/:id/tapes', asyncHandler(async (req, res) => {
     res.redirect(`/users/${userId}/tapes`)
 
 }))
+
+//update a tape name
+router.put('/users/tapes/update', asyncHandler(async (req, res) => {
+    // const userId = req.params.userId;
+    // const tapeId = req.params.tapeId;
+    // const url = req.headers.referer.split('/');
+
+    // console.log(url)
+    // const { tapeName } = req.body;
+    const {userId, tapeId, tapeName} = req.body
+    console.log(tapeId, tapeName, '===================================================')
+
+    const new_tapeName = tapeName;
+
+    // console.log(userId)
+
+    if (new_tapeName) {
+        const tapeToUpdate = await db.Tape.findByPk(Number(tapeId))
+        // await tapeToUpdate.update(new_tapeName)
+        tapeToUpdate.tapeName = new_tapeName
+        await tapeToUpdate.save()
+        return res.render('/login')
+    }else{
+       res.send('Error')
+    }
+}));
+
+
 
 module.exports = router;
